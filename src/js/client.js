97ca62a1fa1f1db278ef0b51a0b366f5ecc9393d
ricsilva93 <ricardospsilva93@hotmail.com>
2017-11-09 18:33:41 +0000

    Yt API and link validation
    integrating youtube api and li elements deleting after click.
    New link validation still not working

--- src/js/client.js
+++ src/js/client.js
@@ -46,23 +46,40 @@ window.addEventListener('DOMContentLoaded', _ => {
 
     //Youtube Video Queue event
     document.getElementById('play').addEventListener('click', _ => {
+        //TODO: error alternatives: "" or invalid link
         if(document.getElementById('youtubeURLToQueue').value === ""){
             return alert('sorry, I don\'t recognize this as a youtube link')
         }
         const ytVideo = document.getElementById('youtubeURLToQueue').value;
-
+            //OLD Link Validation
              ytVideo.replace('watch?v=', 'embed/')
              var validateUrl = ytVideo.replace("watch?v=", "embed/"); //replace to "embed/", so any yt link work
              validateUrl = validateUrl + '?autoplay=1'
              const ytLink = validateUrl; //new const created because replace only works for var
-             console.log('Watching: ' + ytLink)
-             document.getElementById('youtubeFrame').src = ytLink;
+             
+         //NEW VIDEO VALIDATION
+         //-- now players loads video id.
+        /*
+         //extract id from video link
+         var video_id= ytVideo;
+         var video_id = window.location.search.split('v=')[1];
+         var ampersandPosition = video_id.indexOf('&');
+         if(ampersandPosition != -1) {
+           video_id = video_id.substring(0, ampersandPosition); 
+         }*/
+         console.log('Watching: ' + ytVideo)
+            // document.getElementById('youtubeFrame').src = ytLink;
              //  window.frames['youtubeFrame'].location.href.reload() //force refresh iFrame, not needed I guess
 
              const q = document.getElementById('YTqueue')
              const li = document.createElement("li")
              li.appendChild(document.createTextNode(ytLink))
              q.appendChild(li)
+
+             li.onclick = function() {
+                this.parentNode.removeChild(this); //removes the items from the list
+               
+              }
         })
 
     const localL = new LocalList([])
